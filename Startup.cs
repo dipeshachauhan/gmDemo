71c3ac489dd8be68cac2368063e2827ff2698f40
dipesh <dipesh_1999@yahoo.com>
2020-09-27 20:33:21 -0400

    added azure

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace gmDemo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World Azure");
                 });
             });
         }
